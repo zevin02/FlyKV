ce98aa2c912976d2eea28fdc6f7c806d659fea65
Zewen Xu <zevin9427@gmail.com>
2023-08-10 11:39:44 +0800

    Construct LogRecord to manage specified data, and Datafile to manage data files.

---
+++ data/data_file.go
@@ -0,0 +1,25 @@
+package data
+
+import "BitcaskDB/fio"
+
+//数据文件
+type DataFile struct {
+	FileId    uint32        //文件ID
+	WriteOff  uint64        //文件写入到了哪个位置
+	IoManager fio.IOManager //io的读写管理,使用接口，后期可以给多个使用
+}
+
+func OpenDataFile(dirPath string, fieldId uint32) (*DataFile, error) {
+	return nil, nil
+}
+
+func (df *DataFile) ReadLogRecord(offset uint64) (*LogRecord, error) {
+	return nil, nil
+}
+func (df *DataFile) Sync() error {
+	return nil
+}
+
+func (df *DataFile) Write(buf []byte) error {
+	return nil
+}
--- data/log_record.go
+++ data/log_record.go
@@ -5,3 +5,26 @@ type LogRecordPos struct {
 	Fid    uint32 //文件ID，该数据存储在哪个文件中
 	Offset uint64 //偏移，数据存储到数据文件的哪个位置
 }
+
+//将Logrecordtype等价于byte类型，增加可读性质
+type LogRecordType = byte
+
+const (
+	//itoa相当于0,往后进行枚举
+	//Normal：正常写入
+	//Deleted:删除数据
+	LogRecordNormal LogRecordType = itoa
+	LogRecordDeleted
+)
+
+//写入到数据文件的记录,数据文件是追加写入的，类似日志格式
+type LogRecord struct {
+	Key   []byte
+	Value []byte
+	Type  LogRecordType
+}
+
+//对LogRecord进行编码
+func EncodeLogRecord(logRecord *LogRecord) ([]byte, uint64) {
+	return nil, 0
+}
