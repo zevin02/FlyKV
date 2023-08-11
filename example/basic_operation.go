01457f9bf669a7e5fd012bd3dae24ebec6e7239e
Zewen Xu <zevin9427@gmail.com>
2023-08-12 00:15:01 +0800

    add basic operation of DB

---
+++ example/basic_operation.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"BitcaskDB"
+	"fmt"
+)
+
+func main() {
+	opts := BitcaskDB.DefaultOperations
+	//启动一个db实例
+	db, err := BitcaskDB.Open(opts)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Put([]byte("name"), []byte("lily"))
+	if err != nil {
+		panic(err)
+	}
+	err = db.Put([]byte("age"), []byte("10"))
+	if err != nil {
+		panic(err)
+	}
+	val, err := db.Get([]byte("age"))
+	if err != nil {
+		panic(err)
+	}
+	err = db.Delete([]byte("name"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("value=%s", string(val))
+
+}
