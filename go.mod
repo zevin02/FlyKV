55255524738557d63a86f5ab06c5bf0bb2e7eee3
Zewen Xu <zevin9427@gmail.com>
2023-08-18 19:13:59 +0800

    Add test for mmap fioIO
    Signed-off-by: Zewen Xu <zevin9427@gmail.com>

--- go.mod
+++ go.mod
@@ -8,11 +8,12 @@ require (
 	github.com/plar/go-adaptive-radix-tree v1.0.5
 	github.com/stretchr/testify v1.8.4
 	go.etcd.io/bbolt v1.3.7
+	golang.org/x/exp v0.0.0-20230817173708-d852ddb80c63
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ test/mmap_io_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"BitcaskDB/fio"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"path/filepath"
+	"testing"
+)
+
+func TestMMap_Read(t *testing.T) {
+	path := filepath.Join("/tmp", "mmap")
+	//测试完成后将文件删除
+
+	mio, err := fio.NewMMapIOManager(path)
+	assert.Nil(t, err)
+	//文件为空的情况
+	b1 := make([]byte, 10)
+	n, err := mio.Read(b1, 0)
+	assert.Equal(t, 0, n)
+	assert.Equal(t, io.EOF, err)
+	mio.Close()
+
+	//有文件的内容
+	f, err := fio.NewFileIOManager(path)
+	//测试完成后将文件删除
+	defer destroyFile(path)
+	assert.Nil(t, err)
+	assert.NotNil(t, f)
+	f.Write([]byte("aaaaa"))
+	f.Write([]byte("aaaaa"))
+
+	mio1, err := fio.NewMMapIOManager(path)
+	assert.Nil(t, err)
+	size1, err := mio1.Size()
+
+	assert.Equal(t, int64(10), size1)
+	b1 = make([]byte, 10)
+	n, err = mio1.Read(b1, 0)
+	assert.Equal(t, 10, n)
+	assert.Nil(t, err)
+	mio1.Close()
+
+}
